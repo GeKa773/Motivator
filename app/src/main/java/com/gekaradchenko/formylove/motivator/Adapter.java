a02505a8970278c382e226c1f9e7e122dc026cce
GeKaR <GekaRadchenko@gmail.com>
2021-05-20 19:25:22 +0300

    END without style app Commit

---
+++ app/src/main/java/com/gekaradchenko/formylove/motivator/Adapter.java
@@ -0,0 +1,65 @@
+package com.gekaradchenko.formylove.motivator;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.AdapterViewHolder> {
+    private ArrayList<Motivation> motivations = new ArrayList<>();
+
+//    public Adapter(ArrayList<Motivation> motivations) {
+//        this.motivations = motivations;
+//    }
+
+    public ArrayList<Motivation> getMotivations() {
+        return motivations;
+    }
+
+    public void setMotivations(ArrayList<Motivation> motivations) {
+        this.motivations = motivations;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_view_item, parent, false);
+        AdapterViewHolder adapterViewHolder = new AdapterViewHolder(view);
+        return adapterViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
+
+        Motivation motivation = motivations.get(position);
+        holder.textViewID.setText(motivation.getId()+1+":");
+        holder.textViewText.setText(motivation.getText());
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return motivations.size();
+    }
+
+    public class AdapterViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewID;
+        private TextView textViewText;
+
+        public AdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewID = itemView.findViewById(R.id.textViewID);
+            textViewText = itemView.findViewById(R.id.textViewText);
+        }
+    }
+}
---
+++ app/src/main/java/com/gekaradchenko/formylove/motivator/MainActivity.java
@@ -0,0 +1,202 @@
+package com.gekaradchenko.formylove.motivator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private Document document;
+    private Thread secThread;
+    private Runnable runnable;
+
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Motivation> motivations;
+
+    private SharedPreferences sharedPreferences;
+
+    private Date date;
+    private SimpleDateFormat format;
+
+    private String dateFormat = "dd:MM:yyyy";
+
+    private TextView textView;
+    private Button buttonBefore, buttonNext;
+
+    private Boolean boolActivity;
+
+    private Elements all;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        threadStart();
+    }
+
+    private void init() {
+        motivations = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new Adapter();
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        sharedPreferences = getSharedPreferences(getString(R.string.shared_name), MODE_PRIVATE);
+
+        format = new SimpleDateFormat(dateFormat);
+
+        textView = findViewById(R.id.textView);
+        buttonBefore = findViewById(R.id.buttonBefore);
+        buttonNext = findViewById(R.id.buttonNext);
+
+        textView.setVisibility(View.INVISIBLE);
+        buttonBefore.setVisibility(View.INVISIBLE);
+        buttonNext.setVisibility(View.INVISIBLE);
+
+
+        boolActivity = false;
+    }
+
+    private void threadStart() {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                getWeb();
+            }
+        };
+        secThread = new Thread(runnable);
+        secThread.start();
+    }
+
+    private void getWeb() {
+        try {
+            document = Jsoup.connect(getString(R.string.web_site)).get();
+
+            Elements table = document.getElementsByTag("ol");
+            Element ourTable = table.get(0);
+            all = ourTable.children();
+
+            for (int i = 0; i < all.size(); i++) {
+                motivations.add(new Motivation(i, all.get(i).text()));
+            }
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter.setMotivations(motivations);
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void getTodayMotivation(MenuItem item) {
+        checkBool();
+        checkSharedFirst();
+        checkSharedDayToday();
+        Log.d("SSS", "Shared Date: " + sharedPreferences.getString(getString(R.string.shared_day), getString(R.string.shared_first_day)));
+        Log.d("SSS", "SharedID: " + sharedPreferences.getInt(getString(R.string.shared_id), -1));
+
+        setTextView();
+    }
+
+    private void checkBool() {
+        if (boolActivity == true) {
+            boolActivity = false;
+
+            recyclerView.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.INVISIBLE);
+            buttonBefore.setVisibility(View.INVISIBLE);
+            buttonNext.setVisibility(View.INVISIBLE);
+        } else {
+            boolActivity = true;
+
+            recyclerView.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.VISIBLE);
+            buttonBefore.setVisibility(View.VISIBLE);
+            buttonNext.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    private void checkSharedFirst() {
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String date = sharedPreferences.getString(getString(R.string.shared_day), getString(R.string.shared_first_day));
+        int id = sharedPreferences.getInt(getString(R.string.shared_id), -1);
+        if (date.equals(getString(R.string.shared_first_day))) {
+
+            editor.putString(getString(R.string.shared_day), format.format(new Date()));
+            editor.putInt(getString(R.string.shared_id), 0);
+        }
+        if (id >= all.size()) {
+            editor.putInt(getString(R.string.shared_id), 0);
+        }
+        editor.apply();
+    }
+
+    private void checkSharedDayToday() {
+        String dateNow = format.format(new Date());
+        String dataShared = sharedPreferences.getString(getString(R.string.shared_day), getString(R.string.shared_first_day));
+        if (dateNow.equals(dataShared)) {
+            Log.d("SSS", "checkSharedDayToday: equals");
+        } else {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            Log.d("SSS", "checkSharedDayToday: NO NO NO  equals");
+            editor.putString(getString(R.string.shared_day), format.format(new Date()));
+
+            int i = sharedPreferences.getInt(getString(R.string.shared_id), -1);
+            ++i;
+            editor.putInt(getString(R.string.shared_id), i);
+
+            editor.apply();
+
+        }
+    }
+
+    private void setTextView() {
+        int i = sharedPreferences.getInt(getString(R.string.shared_id), -1);
+        if (i == -1) {
+            Toast.makeText(this, "Не получилось загрузить", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        textView.setText(all.get(i).text());
+    }
+}
